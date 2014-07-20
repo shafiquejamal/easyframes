a2c182cdaef973fdb7689d13da9ba46f76ccc6dd
Shafique Jamal <shafique.jamal@gmail.com>
2014-07-20 17:21:01 -0400

    Added license file, moved sample code from test to bin/

---
+++ bin/sample_script.py
@@ -0,0 +1,12 @@
+from easyframes import easyframes
+
+df_original = pd.read_csv('sample_hh_dataset.csv')
+df = df_original.copy()
+
+myhhkit = hhkit()
+df = myhhkit.egen(df, operation='count', groupby='hh', col='hh', column_label='hhsize')
+df = myhhkit.egen(df, operation='mean', groupby='hh', col='age', column_label='mean age in hh')
+df = myhhkit.egen(df, operation='count', groupby='hh', col='hh', column_label='hhs_o22', include=df['age']>22)
+df = myhhkit.egen(df, operation='mean', groupby='hh', col='age')
+
+print(df)
