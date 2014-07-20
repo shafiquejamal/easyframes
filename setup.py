0f617161801b444769306cedd8afe194eb2c2f90
Shafique Jamal <shafique.jamal@gmail.com>
2014-07-20 17:27:10 -0400

    Changed version numbers. Also created setup.py file.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='EasyFrames',
+    version='0.1.0',
+    author='Shafique Jamal',
+    author_email='shafique.jamal@gmail.com',
+    packages=['easyframes', 'easyframes.test'],
+    scripts=['bin/sample_script.py'],
+    url='http://pypi.python.org/pypi/EasyFrames/',
+    license='LICENSE.txt',
+    description='Classes and methods for executing stata-like commands easily for pandas dataframes.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pandas >= 0.14.1",
+        "numpy >= 1.8.1",
+    ],
+)
