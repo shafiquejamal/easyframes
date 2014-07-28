d9ac9566c095b04c4d01aa9fae855c022304e672
Shafique Jamal <shafique.jamal@gmail.com>
2014-07-28 10:15:00 -0400

    No longer need to specify the object when using egen command

--- easyframes/test/test_egen.py
+++ easyframes/test/test_egen.py
@@ -39,7 +39,7 @@ class TestEgen(unittest.TestCase):
 		
 		myhhkit = hhkit()
 		try:
-			df2 = myhhkit.egen(self.df, operation='count', groupby='hh', col='hh', 
+			df2 = myhhkit.egen(operation='count', groupby='hh', col='hh', 
 				exclude=self.my_include, include=self.my_include)
 		except:
 			return True
@@ -48,21 +48,21 @@ class TestEgen(unittest.TestCase):
 	def test_no_include_no_exclude_includes_all_rows(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='count', groupby='hh', col='hh')
+		myhhkit.egen(operation='count', groupby='hh', col='hh')
 		correct_values = pd.Series([3, 3, 3, 1, 2, 2, 4, 4, 4, 4])
 		assert_series_equal(correct_values, myhhkit.df['(count) hh by hh'])
 				
 	def test_specify_include_yields_correct_results_count(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='count', groupby='hh', col='hh', include=self.my_include)
+		myhhkit.egen(operation='count', groupby='hh', col='hh', include=self.my_include)
 		correct_values = pd.Series([np.nan, np.nan, np.nan, 1, 1, 1, 3, 3, 3, 3])
 		assert_series_equal(correct_values, myhhkit.df['(count) hh by hh'])
 
 	def test_specify_include_yields_correct_results_mean(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='mean', groupby='hh', col='age', include=self.my_include)
+		myhhkit.egen(operation='mean', groupby='hh', col='age', include=self.my_include)
 		correct_values = pd.Series([np.nan, np.nan, np.nan, 70, 23, 23, 26.666666, 26.666666, 
 																		26.666666, 26.666666])
 		assert_series_equal(correct_values, myhhkit.df['(mean) age by hh'])
@@ -70,35 +70,35 @@ class TestEgen(unittest.TestCase):
 	def test_specify_exclude_yields_correct_results_count(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='count', groupby='hh', col='hh', exclude=self.my_include)
+		myhhkit.egen(operation='count', groupby='hh', col='hh', exclude=self.my_include)
 		correct_values = pd.Series([3, 3, 3, np.nan, 1, 1, 1, 1, 1, 1])
 		assert_series_equal(correct_values, myhhkit.df['(count) hh by hh'])
 
 	def test_specify_exclude_yields_correct_results_mean(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='mean', groupby='hh', col='age', exclude=self.my_include)
+		myhhkit.egen(operation='mean', groupby='hh', col='age', exclude=self.my_include)
 		correct_values = pd.Series([33.333333, 33.333333, 33.333333, np.nan, 20, 20, 15, 15, 15, 15])
 		assert_series_equal(correct_values, myhhkit.df['(mean) age by hh'])
 
 	def test_using_numeric_exclude_type(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='mean', groupby='hh', col='age', exclude=self.my_include_using_integer)
+		myhhkit.egen(operation='mean', groupby='hh', col='age', exclude=self.my_include_using_integer)
 		correct_values = pd.Series([33.333333, 33.333333, 33.333333, np.nan, 20, 20, 15, 15, 15, 15])
 		assert_series_equal(correct_values, myhhkit.df['(mean) age by hh'])
 
 	def test_using_float_exclude_type(self):
 		myhhkit = hhkit(self.df)
 		# myhhkit.from_dict(self.df)
-		myhhkit.egen(myhhkit, operation='mean', groupby='hh', col='age', exclude=self.my_include_using_float)
+		myhhkit.egen(operation='mean', groupby='hh', col='age', exclude=self.my_include_using_float)
 		correct_values = pd.Series([33.333333, 33.333333, 33.333333, np.nan, 20, 20, 15, 15, 15, 15])
 		assert_series_equal(correct_values, myhhkit.df['(mean) age by hh'])
 
 	def test_using_nonnumeric_exclude_type(self):
 		myhhkit = hhkit()
 		try:
-			df2 = myhhkit.egen(self.df, operation='mean', groupby='hh', col='age', exclude=self.my_include_using_nonnumeric)
+			df2 = myhhkit.egen(operation='mean', groupby='hh', col='age', exclude=self.my_include_using_nonnumeric)
 		except:
 			return True
 		return False
